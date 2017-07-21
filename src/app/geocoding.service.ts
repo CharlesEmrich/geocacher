cbf95e37c9187321e54e66b8eba40edf7d6e2725
Charles Emrich <libertinistic@gmail.com>
2017-07-21 12:18:22 -0700

    begin building out service

--- src/app/geocoding.service.ts
+++ src/app/geocoding.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+
+import { geoKey } from './api-keys';
 
 @Injectable()
 export class GeocodingService {
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) { }
 
+  getAddress(lat: string, long: string) {
+
+  }
+
+  getLatLong(address: string) {
+    
+  }
 }
