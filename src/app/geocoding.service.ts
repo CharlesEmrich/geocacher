9f23c1caa9d749df8601daa336fa1596f359ac41
Charles Emrich <libertinistic@gmail.com>
2017-07-21 12:23:39 -0700

    add methods to geocode service

--- src/app/geocoding.service.ts
+++ src/app/geocoding.service.ts
@@ -11,11 +11,11 @@ export class GeocodingService {
     private http: Http
   ) { }
 
-  getAddress(lat: string, long: string) {
-
+  getAddress(lat: string, long: string): Observable<any> {
+    return this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?key=${geoKey}&latlng=${lat},${long}`);
   }
 
-  getLatLong(address: string) {
-    
+  getLatLong(address: string): Observable<any> {
+    return this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?key=${geoKey}&address=${address}`);
   }
 }
