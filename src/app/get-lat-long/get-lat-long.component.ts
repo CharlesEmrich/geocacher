81167fe394bf898cc67b2d0f993adb316ef5bcf7
Charles Emrich <libertinistic@gmail.com>
2017-07-21 13:18:48 -0700

    build out get-address component and wrestle bower init in powershell"

--- src/app/get-lat-long/get-lat-long.component.ts
+++ src/app/get-lat-long/get-lat-long.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GeocodingService } from './../geocoding.service';
 
 @Component({
   selector: 'app-get-lat-long',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./get-lat-long.component.css']
 })
 export class GetLatLongComponent implements OnInit {
+  latitude: string;
+  longitude: string;
+  addressObj: string;
 
-  constructor() { }
+  constructor(
+    private geocodingService: GeocodingService
+  ) { }
 
   ngOnInit() {
+  }
+
+  getAddress(userLat: string, userLong: string) {
+    this.latitude = userLat;
+    this.longitude = userLong;
+    this.geocodingService.getAddress(userLat, userLong).subscribe(response => {
+      console.log(response.json().results);
+      this.addressObj = response.json().results;
+    });
   }
 
 }
